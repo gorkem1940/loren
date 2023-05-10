430568b24ec869f09562dd24dbca23b9344fd37f
gorkem <113816295+gorkem1940@users.noreply.github.com>
2023-05-10 10:14:52 +0300

    Update server.js

--- src/commands/server.js
+++ src/commands/server.js
@@ -3,9 +3,9 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("sahip")
-    .setDescription(".gg/1940"),
+    .setName("sunucu")
+    .setDescription("şuan sunucu bulunmamaktadır."),
     run: async (client, interaction) => {
-      interaction.reply(`discord.gg/1940`)
+      interaction.reply(`şuan sunucu bulunmamaktadır.`)
     }
  };
