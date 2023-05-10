35d4be673d5fa1f275749f8a1171e2c0984f1231
gorkem <113816295+gorkem1940@users.noreply.github.com>
2023-05-10 10:13:45 +0300

    Update sahip.js

--- src/commands/sahip.js
+++ src/commands/sahip.js
@@ -4,8 +4,8 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("sahip")
-    .setDescription("tanrisiz visa#1940"),
+    .setDescription("gorkem"),
     run: async (client, interaction) => {
-      interaction.reply(`tanrisiz visa#1940`)
+      interaction.reply(`gorkem`)
     }
  };
